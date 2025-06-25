6ecf1cb9bb186632127be26747635ac8a02637c8
Moimaritim <moimaritim@gmail.com>
2025-06-25 09:53:37 +0300

    quotes fix

--- utils/redis.js
+++ utils/redis.js
@@ -1,14 +1,14 @@
-import redis from "redis";
+import redis from 'redis';
 
 class RedisClient {
   constructor() {
     this.client = redis.createClient();
 
-    this.client.on("error", (err) => {
-      console.error("Redis Client Error:", err.message);
+    this.client.on('error', (err) => {
+      console.error('Redis Client Error:', err.message);
     });
 
-    this.client.on("connect", () => {
+    this.client.on('connect', () => {
       this.connected = true;
     });
   }
@@ -21,7 +21,7 @@ class RedisClient {
     return new Promise((resolve) => {
       this.client.get(key, (err, value) => {
         if (err) {
-          console.error("Redis GET Error:", err.message);
+          console.error('Redis GET Error:', err.message);
           resolve(null);
         } else {
           resolve(value);
@@ -34,7 +34,7 @@ class RedisClient {
     return new Promise((resolve) => {
       this.client.setex(key, duration, value, (err) => {
         if (err) {
-          console.error("Redis SET Error:", err.message);
+          console.error('Redis SET Error:', err.message);
         }
         resolve();
       });
@@ -45,7 +45,7 @@ class RedisClient {
     return new Promise((resolve) => {
       this.client.del(key, (err) => {
         if (err) {
-          console.error("Redis DEL Error:", err.message);
+          console.error('Redis DEL Error:', err.message);
         }
         resolve();
       });
