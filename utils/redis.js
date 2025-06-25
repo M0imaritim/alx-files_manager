df2c6dc85236d9207f2cde2eb70f9f781709272b
Moimaritim <moimaritim@gmail.com>
2025-06-25 08:19:09 +0300

    initial commit

---
+++ utils/redis.js
@@ -0,0 +1,54 @@
+import { createClient } from 'redis';
+
+class RedisClient {
+  constructor() {
+    this.client = createClient();
+    this.client.on('error', (err) => {
+      console.error('Redis Client Error:', err);
+    });
+
+    this.client.connect()
+      .then(() => {
+        this.isReady = true;
+      })
+      .catch((err) => {
+        console.error('Redis Connection Error:', err);
+        this.isReady = false;
+      });
+  }
+
+  isAlive() {
+    return this.client.isReady;
+  }
+
+  async get(key) {
+    try {
+      const value = await this.client.get(key);
+      return value;
+    } catch (err) {
+      console.error('Redis GET Error:', err);
+      return null;
+    }
+  }
+
+  async set(key, value, duration) {
+    try {
+      await this.client.set(key, value, {
+        EX: duration,
+      });
+    } catch (err) {
+      console.error('Redis SET Error:', err);
+    }
+  }
+
+  async del(key) {
+    try {
+      await this.client.del(key);
+    } catch (err) {
+      console.error('Redis DEL Error:', err);
+    }
+  }
+}
+
+const redisClient = new RedisClient();
+export default redisClient;
